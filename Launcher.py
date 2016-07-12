c3d8eb82a6eae9fe81a950f81dad1c01e848d429
Bear E. Funny <bearefunny@gmail.com>
2016-07-12 19:36:02 -0400

    launcher: Switch from Tk to Panda
    I tried to make everything look decent; if you still think it's ugly
    just lmk

--- Launcher.py
+++ Launcher.py
@@ -1,39 +1,59 @@
-from Tkinter import *
-import threading
+from panda3d.core import *
+
+loadPrcFileData('', 'window-title Toontown\'s Funny Farm')
+loadPrcFileData('', 'win-size 600 375')
+loadPrcFileData('', 'win-fixed-size 1')
+loadPrcFileData('', 'icon-filename resources/phase_3/etc/funnyfarm.ico')
+
+from direct.showbase.ShowBase import ShowBase
+
+ShowBase()
+
+from direct.gui.DirectGui import *
+
+DGG.setDefaultFont(loader.loadFont('resources/phase_3/models/fonts/ImpressBT.ttf'))
+
+from direct.interval.IntervalGlobal import *
 from subprocess import Popen
 import sys
 
 class Launcher:
+    version = '1.0.0'
 
     def __init__(self):
-        self.window = Tk()
-        self.window.resizable(False, False)
-        self.window.geometry('300x100+810+490')
-        self.window.title('Toontown\'s Funny Farm')
-        self.frame = Frame(self.window)
-        self.frame.pack()
-        self.text = Label(self.window, text='Checking for updates...', height=5, font=('Helvetica', 12))
-        self.text.pack()
+        background = loader.loadTexture('resources/phase_3/maps/launcher_gui.jpg')
+        self.frame = DirectFrame(parent=render2d, relief=None, image=background)
+        self.versionText = DirectLabel(parent=base.a2dTopLeft, relief=None, text='Funny Farm Launcher v%s' % self.version, pos=(0.5, 0, -0.1), scale=0.08, text_fg=(1.0, 1.0, 1.0, 1.0))
+        self.text = DirectLabel(relief=None, text='', pos=(0, 0, -0.5), scale=0.15, text_fg=(1.0, 1.0, 1.0, 1.0))
 
     def checkForUpdates(self):
+        self.text['text'] = 'Checking for updates...'
         # placeholder code for now, we'll download stuff later
-        threading.Timer(2, self.exit).start()
+        Sequence(Wait(2), Func(self.startGame)).start()
+
+    def updateFiles(self):
+        self.text['text'] = 'Updating file n of n'
 
     def exit(self):
-        self.window.destroy()
-        self.window.quit()
-        del self.window
+        self.frame.destroy()
+        self.frame.removeNode()
+        self.versionText.destroy()
+        self.versionText.removeNode()
+        self.text.destroy()
+        self.text.removeNode()
         del self.frame
+        del self.versionText
         del self.text
-        self.startGame()
-        sys.exit()
+        base.closeWindow(base.win)
 
     def startGame(self):
         # This is where we would execute FFEngine.exe.
         # So technically, since all this launcher does is check for updates,
         # the user could just execute FFEngine.exe and the game will run fine; it just would not have checked for updates
-        p = Popen('StartGame.bat')
+        self.exit()
+        Popen('StartGame.bat')
 
 launcher = Launcher()
 launcher.checkForUpdates()
-launcher.window.mainloop()
+
+base.run()
