67c7e8e0ab963f1c40acaeb861463947b35d0e49
Bear E. Funny <bearefunny@gmail.com>
2016-12-31 13:21:30 -0500

    npcs: Fix a couple bugs in buildings
    Fixes the weird camera movement when you talk to them, and fixes a crash
    if you exited while they were talking.

--- toontown/toon/NPCToon.py
+++ toontown/toon/NPCToon.py
@@ -95,7 +95,7 @@ class NPCToon(NPCToonBase):
         self.detectAvatars()
         self.initPos()
         if isLocalToon:
-            base.localAvatar.posCamera(0, 0)
+            #base.localAvatar.posCamera(0, 0)
             taskMgr.remove(self.uniqueName('clearMovie'))
             self.clearMovie(None)
             self.nametag3d.clearDepthTest()
@@ -135,8 +135,8 @@ class NPCToon(NPCToonBase):
             rejectString = self.chooseQuestDialogReject()
             rejectString = self.fillInQuestNames(rejectString, avName=base.avatarData.setName)
             self.setChatAbsolute(rejectString, CFSpeech | CFTimeout)
-            if isLocalToon:
-                base.localAvatar.posCamera(0, 0)
+            #if isLocalToon:
+                #base.localAvatar.posCamera(0, 0)
             return
         if mode == NPCToons.QUEST_MOVIE_TIER_NOT_DONE:
             rejectString = self.chooseQuestDialogTierNotDone()
