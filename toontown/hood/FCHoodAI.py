98ef56e3966f209588a6e8279474c83cdf6154e5
Bear E. Funny <bearefunny@gmail.com>
2017-06-21 11:24:06 -0400

    general: Goodbye Funny Farm Central
    It had to be done. This also removes the secret area.

--- toontown/hood/FCHoodAI.py
+++
@@ -1,7 +0,0 @@
-from toontown.toonbase import FunnyFarmGlobals
-from HoodAI import HoodAI
-
-class FCHoodAI(HoodAI):
-
-    def __init__(self, air):
-        HoodAI.__init__(self, air, FunnyFarmGlobals.FunnyFarmCentral)
