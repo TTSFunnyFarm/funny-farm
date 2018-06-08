cacd54d377c3e0f6b3d394506e5bd6d653a2b0e5
John Cote <jwcotejr@gmail.com>
2018-06-07 23:11:38 -0400

    toon: PandaModules is deprecated

--- toontown/toon/NPCClerk.py
+++ toontown/toon/NPCClerk.py
@@ -1,4 +1,4 @@
-from pandac.PandaModules import *
+from panda3d.core import *
 from NPCToonBase import *
 from toontown.minigame import ClerkPurchase
 from toontown.book.PurchaseManagerConstants import *
