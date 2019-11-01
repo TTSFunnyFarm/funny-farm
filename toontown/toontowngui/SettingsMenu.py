c1ed074cedd27f292a13c0dae905bdc6b6481dcc
Nathan S <nsinvisblemarions@gmail.com>
2019-11-01 19:25:53 -0400

    SettingsMenu: some formatting

--- toontown/toontowngui/SettingsMenu.py
+++ toontown/toontowngui/SettingsMenu.py
@@ -23,7 +23,7 @@ class SettingsMenu(DirectFrame):
         DirectFrame.__init__(self, aspect2d, relief=None)
         self.initialiseoptions(SettingsMenu)
         self.categoryNames = ["Audio", "Video", "Controls", "Extras"]
-        self.title = DirectLabel(parent=self, relief=None, text_scale=(0.1, 0.1, 1.0), text=self.categoryNames[0], text_font=ToontownGlobals.getMinnieFont(), text_pos=(0, 0.43, 0), text_fg=(0.24, 0.13, 0.008, 1), text_align=TextNode.ACenter)
+        self.title = DirectLabel(parent=self, relief=None, text_scale=(0.1, 0.1, 1.0), text=self.categoryNames[0], text_font=ToontownGlobals.getMinnieFont(), text_pos=(0, 0.23, 0), text_fg=(0.24, 0.13, 0.008, 1), text_align=TextNode.ACenter)
         buttonIcons = [self.settingsGui.find('**/settingsAudio'), self.settingsGui.find('**/settingsVideo'), self.settingsGui.find('**/settingsControls'), self.settingsGui.find('**/settingsExtra')]
         self.hover = self.settingsGui.find('**/settingsHover')
         self.hover = OnscreenImage(image = self.hover, pos = (0, 0, 0))
@@ -36,7 +36,7 @@ class SettingsMenu(DirectFrame):
         for icon in buttonIcons:
             i += 1
             self.categories[i] = {}
-            button = DirectButton(parent=self, relief=None, pos=(i * 0.22 - 0.28, 0, 0.3), image_scale=(1.0, 1.0, 1.0), state=DGG.NORMAL, image=buttonIcons[i], command=self.switchCategory, extraArgs=[i])
+            button = DirectButton(parent=self, relief=None, pos=(i * 0.22 - 0.28, 0, 0.43), image_scale=(1.0, 1.0, 1.0), state=DGG.NORMAL, image=buttonIcons[i], command=self.switchCategory, extraArgs=[i])
             button.bind(DGG.WITHIN, self._onHover, [button])
             button.bind(DGG.WITHOUT, self._onExit, [button])
         guiButton = loader.loadModel('phase_3/models/gui/quit_button')
@@ -44,9 +44,9 @@ class SettingsMenu(DirectFrame):
         self.dialog = TTDialog.TTDialog(text="Are you sure you want to apply these settings?", text_align=TextNode.ACenter, style=TTDialog.YesNo, command=self.handleSettingsSave)
         category = self.categories[AUDIO]
         category["musicVol"] = DirectSlider(parent=self, scale=(0.4,1.0,0.65), pos=(0.47, 0, 0.07), range=(0,100), value=settings["musicVol"] * 100, pageSize=0, color=(0.33, 0.2, 0.031,255), thumb_relief=None, thumb_image=(guiButton.find('**/QuitBtn_UP'), guiButton.find('**/QuitBtn_DN'), guiButton.find('**/QuitBtn_RLVR')), thumb_color=(1.0,1.0,1.0,255), command=self._onMusicVolumeUpdate)
-        category["musicVolLabel"] = DirectLabel(parent=self, relief=None, text="", text_scale=(0.1, 0.1, 1.0), text_pos=(-0.4, 0.05, 0))
+        category["musicVolLabel"] = DirectLabel(parent=self, relief=None, text="", text_fg=(0.24, 0.13, 0.008, 1), text_scale=(0.1, 0.1, 1.0), text_pos=(-0.4, 0.05, 0))
         category["sfxVol"] = DirectSlider(parent=self, scale=(0.4,1.0,0.65), pos=(0.47, 0, -0.10), range=(0,100), value=settings["sfxVol"] * 100, pageSize=0, color=(0.33, 0.2, 0.031,255), thumb_relief=None, thumb_image=(guiButton.find('**/QuitBtn_UP'), guiButton.find('**/QuitBtn_DN'), guiButton.find('**/QuitBtn_RLVR')), thumb_color=(1.0,1.0,1.0,255), command=self._onSFXVolumeUpdate)
-        category["sfxVolLabel"] = DirectLabel(parent=self, relief=None, text="", text_scale=(0.1, 0.1, 1.0), text_pos=(-0.4, -0.13, 0))
+        category["sfxVolLabel"] = DirectLabel(parent=self, relief=None, text="", text_fg=(0.24, 0.13, 0.008, 1), text_scale=(0.1, 0.1, 1.0), text_pos=(-0.4, -0.13, 0))
         self.oldSettings = settings
         self.setBin('gui-popup', 0)
 
