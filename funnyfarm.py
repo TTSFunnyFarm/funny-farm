f80a169b0a24b0b090bc5e42b65bad9f4a839aff
John Cote <jwcotejr@users.noreply.github.com>
2019-12-14 22:35:10 -0500

    Update funnyfarm.py

--- funnyfarm.py
+++ funnyfarm.py
@@ -1,1 +1,5 @@
+# This is the "main" file used by compiled clients to start the game.
+# It pretty much just imports FunnyFarmStart, but attempting to use
+# FunnyFarmStart as the "main" file results in FunnyFarmStart not being
+# found for some reason.
 from toontown.toonbase import FunnyFarmStart
