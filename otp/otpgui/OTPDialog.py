95484aeeeafaaf203b5ead4df53f9d5d2dc06774
John Cote <jwcotejr@gmail.com>
2019-12-22 23:38:27 -0500

    Update OTPDialog.py

--- otp/otpgui/OTPDialog.py
+++ otp/otpgui/OTPDialog.py
@@ -69,9 +69,7 @@ class OTPDialog(DirectDialog):
          ('button_relief', None, None),
          ('button_text_pos', (0, -0.1), None),
          ('fadeScreen', 0.5, None),
-         ('image_color', OTPGlobals.GlobalDialogColor, None),
-         ('image', DGG.getDefaultDialogGeom(), None),
-         ('relief', None, None))
+         ('image_color', OTPGlobals.GlobalDialogColor, None))
         self.defineoptions(kw, optiondefs)
         DirectDialog.__init__(self, parent)
         self.initialiseoptions(OTPDialog)
