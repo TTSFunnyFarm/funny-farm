f8a095d2b833ee76ce0ac670c8d01abbc2f80ca9
John Cote <jwcotejr@users.noreply.github.com>
2019-12-22 22:51:49 -0500

    Revert "Update funnyfarm.py"
    This reverts commit f80a169b0a24b0b090bc5e42b65bad9f4a839aff.

--- funnyfarm.py
+++ funnyfarm.py
@@ -1,5 +1,1 @@
-# This is the "main" file used by compiled clients to start the game.
-# It pretty much just imports FunnyFarmStart, but attempting to use
-# FunnyFarmStart as the "main" file results in FunnyFarmStart not being
-# found for some reason.
 from toontown.toonbase import FunnyFarmStart
