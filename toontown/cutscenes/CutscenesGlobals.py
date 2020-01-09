7399f3f579357c025dd517e3fd54fbeaa3083651
Nathan S <nathanstgeorge7@gmail.com>
2020-01-08 22:50:19 -0500

    Update CutscenesGlobals.py

--- toontown/cutscenes/CutscenesGlobals.py
+++ toontown/cutscenes/CutscenesGlobals.py
@@ -1,3 +1,4 @@
+from toontown.cutscenes import *
 TOON = 0
 COG = 1
 
@@ -12,8 +13,8 @@ ANGRY_MUZZLE = 2
 SMILE_MUZZLE = 3
 LAUGH_MUZZLE = 4
 SURPRISE_MUZZLE = 5
-Cutscenes = (1,
- 1001,
- 1002,
- 1004,
- 1028)
+Cutscenes = (1, #nice magic number
+ IntroScene.IntroScene.id,
+ FlippySuitIntroScene.FlippySuitIntroScene.id,
+ MeterDisasterScene.MeterDisasterScene.id,
+ FirstBuildingScene.FirstBuildingScene.id)
