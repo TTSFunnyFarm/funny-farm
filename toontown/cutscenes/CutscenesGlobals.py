7683f0509a9bb3929240ee6b5a7e3af3a226f494
Nathan S <nathanstgeorge7@gmail.com>
2020-01-09 17:07:37 -0500

    Revert "Update CutscenesGlobals.py"
    This reverts commit 7399f3f579357c025dd517e3fd54fbeaa3083651.

--- toontown/cutscenes/CutscenesGlobals.py
+++ toontown/cutscenes/CutscenesGlobals.py
@@ -1,4 +1,3 @@
-from toontown.cutscenes import *
 TOON = 0
 COG = 1
 
@@ -13,8 +12,8 @@ ANGRY_MUZZLE = 2
 SMILE_MUZZLE = 3
 LAUGH_MUZZLE = 4
 SURPRISE_MUZZLE = 5
-Cutscenes = (1, #nice magic number
- IntroScene.IntroScene.id,
- FlippySuitIntroScene.FlippySuitIntroScene.id,
- MeterDisasterScene.MeterDisasterScene.id,
- FirstBuildingScene.FirstBuildingScene.id)
+Cutscenes = (1,
+ 1001,
+ 1002,
+ 1004,
+ 1028)
