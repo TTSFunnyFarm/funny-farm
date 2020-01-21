eb2f049a48cb902c88c390272884450beccbb056
Michael Keller <mkeller2013@fau.edu>
2020-01-20 20:35:02 -0500

    Cog walks around in circle

--- toontown/hood/DebugLand.py
+++ toontown/hood/DebugLand.py
@@ -15,6 +15,19 @@ class DebugLand(ToonHood):
         self.spookyHoodFile = 'phase_14/models/debug/thebug_land'
         self.titleColor = (0.5, 0.5, 0.5, 1.0)
 
+    def startSuitWalkInterval(self):
+        self.suitWalk = Sequence(
+            Func(self.suit.setHpr, 90, 0, 0),
+            self.suit.posInterval(20, self.suitPoints[0]),
+            Func(self.suit.setHpr, 0, 0, 0),
+            self.suit.posInterval(15, self.suitPoints[1]),
+            Func(self.suit.setHpr, 270, 0, 0),
+            self.suit.posInterval(20, self.suitPoints[2]),
+            Func(self.suit.setHpr, 180, 0, 0),
+            self.suit.posInterval(15, self.suitPoints[3])
+        )
+        self.suitWalk.loop()
+
     def enter(self, shop=None, tunnel=None, init=0):
         ToonHood.enter(self, shop=shop, tunnel=tunnel, init=init)
         suitDNA = SuitDNA.SuitDNA()
@@ -23,10 +36,18 @@ class DebugLand(ToonHood):
         self.suit.setDNA(suitDNA)
         self.suit.setLevel(0)
         self.suit.reparentTo(render)
-        self.suit.setPos(505, -97, -0.097)
-        self.suit.setPosHpr(505, -97, -0.097, 90, 0, 0)
+        self.suit.setPos(650, -100, 0.1)
+        self.suit.setPosHpr(650, -100, 0.1, 90, 0, 0)
         self.suit.addActive()        
-
+        self.suitPoints = [
+            Point3(450, -100, 0.1),                     
+            Point3(450, 50, 0.3),
+            Point3(650, 50, 0.3),
+            Point3(650, -100, 0.1)   
+        ]
+        self.suit.loop('walk')
+        self.startSuitWalkInterval()
+    
     def exit(self):
         ToonHood.exit(self)
 
@@ -35,3 +56,5 @@ class DebugLand(ToonHood):
 
     def unload(self):
         ToonHood.unload(self)
+    
+    
