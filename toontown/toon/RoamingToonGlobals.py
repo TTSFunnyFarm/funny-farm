77e51a476d09693248ce7eb6d047d3801d27322b
Nathan S <nathanstgeorge7@gmail.com>
2020-05-10 15:06:25 -0400

    toon-rtg: add some phrases

--- toontown/toon/RoamingToonGlobals.py
+++ toontown/toon/RoamingToonGlobals.py
@@ -2,3 +2,9 @@ MAX_ACCELERATION = 5
 MAX_SPEED = 8
 WALK_THRESHOLD = 0.1
 RUN_THRESHOLD = 1
+ChatterPhrases = ["It sure is sunny out today!",
+ "These gags are getting heavy...",
+ "I could go for a good joke."]
+
+InteractPhrases = ["Hey %s, how's it hangin'?",
+ "Have a Toontastic day!"]
