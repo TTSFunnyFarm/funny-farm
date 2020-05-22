c56ed3fba61aae074add12d4f9cd1afa59242c40
Nathan S <nathanstgeorge7@gmail.com>
2020-05-22 13:55:28 -0400

    proto: laff meter is a thing now

--- prototypes/combat/ProtoGame.py
+++ prototypes/combat/ProtoGame.py
@@ -1,4 +1,4 @@
-from toontown.toon import Toon, ToonDNA, ToonHead
+from toontown.toon import LocalToon, Toon, ToonDNA, ToonHead, LaffMeter
 from direct.showbase.DirectObject import DirectObject
 from direct.directnotify import DirectNotifyGlobal
 
@@ -12,13 +12,21 @@ class ProtoGame(DirectObject):
         dna.newToonRandom()
         self.geom = loader.loadModel('phase_5/models/cogdominium/tt_m_ara_crg_penthouse')
         self.geom.reparentTo(render)
-        self.toon = Toon.Toon()
+        self.toon = LocalToon.LocalToon()
         self.toon.setDNA(dna)
         self.toon.useLOD(1000)
         self.toon.startBlink()
         self.toon.startLookAround()
         self.toon.reparentTo(render)
         self.toon.loop('neutral')
+        self.laffMeter = LaffMeter.LaffMeter(self.toon.style, self.toon.hp, self.toon.maxHp)
+        self.laffMeter.setAvatar(self.toon)
+        self.laffMeter.setScale(0.075)
+        self.laffMeter.reparentTo(base.a2dBottomLeft)
+        if self.toon.style.getAnimal() == 'monkey':
+            self.laffMeter.setPos(0.153, 0.0, 0.13)
+        else:
+            self.laffMeter.setPos(0.133, 0.0, 0.13)
 
     def preload(self):
         self.notify.info('Preloading things...')
