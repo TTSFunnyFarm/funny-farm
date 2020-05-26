81a9f40c14da11710dd4df9ddcfb7c0746fb4514
Nathan S <nathanstgeorge7@gmail.com>
2020-05-26 16:09:53 -0400

    proto: you can switch tracks now

--- prototypes/combat/GagMenu.py
+++ prototypes/combat/GagMenu.py
@@ -35,9 +35,12 @@ class GagMenu(DirectFrame):
     def __init__(self):
         DirectFrame.__init__(self, relief=None)
         bg = CreateCircle()
+        matGui = loader.loadModel('phase_3/models/gui/tt_m_gui_mat_mainGui')
         self.bg = bg
         self.bg.reparentTo(self)
         self.trackLabel = DirectLabel(parent=self, relief=None, pos=(0, 0, 0), text='', text_scale=0.12, text_fg=Vec4(1, 1, 1, 1), text_align=TextNode.ACenter, text_font=FunnyFarmGlobals.getMinnieFont(), text_shadow=(0, 0, 0, 1))
+        self.rightArrow = DirectButton(parent=self, relief=None, image=(matGui.find('**/tt_t_gui_mat_nextUp'), matGui.find('**/tt_t_gui_mat_nextDown'), matGui.find('**/tt_t_gui_mat_nextUp'), matGui.find('**/tt_t_gui_mat_nextDisabled')), pos=(-0.05, 0, 0.025), image_scale=(0.18, 0.18, 0.18), image1_scale=(0.21, 0.21, 0.21), image2_scale=(0.21, 0.21, 0.21), image3_scale=(0.18, 0.18, 0.18), command=self.__changeTrack, extraArgs=[1])
+        self.leftArrow = DirectButton(parent=self, relief=None, image=(matGui.find('**/tt_t_gui_mat_nextUp'), matGui.find('**/tt_t_gui_mat_nextDown'), matGui.find('**/tt_t_gui_mat_nextUp'), matGui.find('**/tt_t_gui_mat_nextDisabled')), pos=(0.05, 0, 0.025), image_scale=(-0.18, 0.18, 0.18), image1_scale=(-0.21, 0.21, 0.21), image2_scale=(-0.21, 0.21, 0.21), image3_scale=(-0.18, 0.18, 0.18), command=self.__changeTrack, extraArgs= [-1])
         invModel = loader.loadModel('phase_3.5/models/gui/inventory_icons')
         self.invModels = []
         self.gagButtons = []
@@ -49,6 +52,9 @@ class GagMenu(DirectFrame):
             self.invModels.append(itemList)
         invModel.removeNode()
         self.setTrack(1)
+
+    def __changeTrack(self, delta):
+        self.setTrack((self.track + delta) % 7)
 
     def setTrack(self, track):
         for button in self.gagButtons:
@@ -62,6 +68,10 @@ class GagMenu(DirectFrame):
             a = rads * i / trackLen + 1
             y = math.sin(a) * 0.4
             x = math.cos(a) * 0.4
-            button = DirectButton(parent=self, relief=None, image_pos=(x,0,y), image=gagModel, image_scale=((1.2,1,1.2),(0.5,1,0.5), (0.75,1,0.75)))
-        self.bg.setColor(Vec4(*ToontownBattleGlobals.TrackColors[1], 0.9))
+            button = DirectButton(parent=self, relief=None, image_pos=(x,0,y), image=gagModel, image_scale=(1,1,1))
+            self.gagButtons.append(button)
+        self.bg.setColor(Vec4(*ToontownBattleGlobals.TrackColors[track], 0.9))
         self.trackLabel['text'] = TTLocalizer.BattleGlobalTracks[track]
+        trackLen = len(self.trackLabel['text'])
+        self.leftArrow.setX(-(0.05 * trackLen))
+        self.rightArrow.setX(0.05 * trackLen)
