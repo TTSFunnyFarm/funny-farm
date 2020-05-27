7e37e8b671d870af6b8f19873cf06b4af1bf8c61
Nathan S <nathanstgeorge7@gmail.com>
2020-05-27 06:08:44 -0400

    proto: some buttons on the attack menu

--- prototypes/combat/AttackMenu.py
+++ prototypes/combat/AttackMenu.py
@@ -2,6 +2,12 @@ from direct.gui.DirectGui import *
 from panda3d.core import *
 class AttackMenu(DirectFrame):
     def __init__(self):
+        guiButton = loader.loadModel('phase_3/models/gui/quit_button')
         DirectFrame.__init__(self, relief=None, geom=DGG.getDefaultDialogGeom(), geom_scale=(1, 1, 0.5), pos=(0, 0, 0))
         self.initialiseoptions(AttackMenu)
+        buttonImage = (guiButton.find('**/QuitBtn_UP'), guiButton.find('**/QuitBtn_DN'), guiButton.find('**/QuitBtn_RLVR')),
+        buttonScale = (0.91, 1, 1.3)
+        self.cancelButton = DirectButton(parent=self, relief=None, image=buttonImage, image_scale=buttonScale, text="Attack", pos=(-0.26, 0, 0.12), text_scale=0.08, text_pos=(0, -0.02), command=print)
+        self.runButton = DirectButton(parent=self, relief=None, image=buttonImage, image_scale=buttonScale, text="Run", pos=(0.2, 0, -0.12), text_scale=0.08, text_pos=(0, -0.02), command=print)
+        guiButton.removeNode()
         #self.setBin('gui-popup', 0)
